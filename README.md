a7e205372dd207a7ccdac2e1b1aa128c1fb5a178
mi-ano <ryanmiano68@gmail.com>
2019-06-01 21:43:02 +0300

    polished readme

--- README.md
+++ README.md
@@ -10,5 +10,22 @@ This is a project that describes a triangle whether it is either Equilateral,Sca
 
 This program can only be accessed on a computer with Git and Atom/Visual Studio Code installed.
 
+* Access my Github Account
 * Clone this repository
 * Open index.html in Terminal
+
+## Technologies Used
+The technologies used in making of this able project are;
+* HTML
+* CSS
+* BOOTSTRAP
+* JAVASCRIPT 
+
+## Known Bugs
+So far i haven't come across any known bugs.If you come across by please feel free to contact me.
+
+## Contact Information
+YOu can contact me through my E-mail;ryanmiano68@gmail.com.
+
+## License and Copyright Information
+The project has an MIT License therefore it can be used by anyone for whichever purpose they deem fit to use it under. Copyright (c) {2019} Ryan Miano.
--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,17 @@ function tracker() {
     triangle[1] = document.getElementById('s2').value;
     triangle[2] = document.getElementById('s3').value;
 
-            if ( (triangle[0]+triangle[1]) <= triangle[2] || (triangle[1]+triangle[2]) <= triangle[0] || (triangle[2]+triangle[0]) <= triangle[1] ) {
-          alert("triangle not confirmed");
-        }
-        else if ( triangle[0] === triangle[1] && triangle[1] === triangle[2] && triangle[2] === triangle[0] ) {
-          /* alert("Equilateral triangle confirmed"); */
-          document.getElementById('type').innerHTML = "<h2>This is an Equilateral triangle</h2> <p>This triangle has same lengths for all sides of the triangle. The sides intersect at angle of 60 degrees to give a total of 180 degrees</p>";
-        }
-        else if ( triangle[0] === triangle[1] || triangle[1] === triangle[2] || triangle[2] === triangle[0] ) {
-          /* alert("Isosceles triangle confirmed"); */
-          document.getElementById('type').innerHTML = "<h2>This is an Isosceles triangle</h2> <p>This triangle has two sides with the same values for length but one side with a different length</p>";
-        }
-        else if ( (triangle[0]+triangle[1]) >= triangle[2] || (triangle[1]+triangle[2]) >= triangle[0] || (triangle[2]+triangle[0]) >= triangle[1]){
-          /* alert("Scalene triangle confirmed"); */
-          document.getElementById('type').innerHTML = "<h2>This is a Scalene triangle</h2> <p>The sides of this triangle all have different lengths and the may be further divided into Right angled triangles and obtuse triangles among others</p>";
-        }
-        else {
-          /* alert("Please try again"); */
-          document.getElementById('type').innerHTML = "<h2>Please try again</h2>";
-        }
-
+    if ( (triangle[0]+triangle[1]) <= triangle[2] || (triangle[1]+triangle[2]) <= triangle[0] || (triangle[2]+triangle[0]) <= triangle[1] ) {
+        alert("cannot form a triangle");
+    }else if ( triangle[0] === triangle[1] && triangle[1] === triangle[2] && triangle[2] === triangle[0] ) {
+        document.getElementById('type').innerHTML = "<P>this is an equilateral triangle. All side sof the triangle are equal and the angles are 60.</P>";
+    }else if ( triangle[0] === triangle[1] || triangle[1] === triangle[2] || triangle[2] === triangle[0] ) {
+        document.getElementById('type').innerHTML = "<p>this is an isosceles triangle. 2 sides and 2 angles are equal</p>";
+    }else if ( (triangle[0]+triangle[1]) >= triangle[2] || (triangle[1]+triangle[2]) >= triangle[0] || (triangle[2]+triangle[0]) >= triangle[1]){
+        document.getElementById('type').innerHTML = "<p>this is a scalene triangle no sides a equal and none of the angles are equal</p>";
+    }else {
+        document.getElementById('type').innerHTML = "<h2>Please try again</h2>";
+    }
 }
 function trianglereset() {
 document.getElementById('type').innerHTML = "";
