b32da2c503a0c98bfbe4ce9be42f74639eb527c6
mi-ano <ryanmiano68@gmail.com>
2019-06-02 14:08:16 +0300

    js complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 var triangle = [];
 
 function tracker() {
-    triangle[0] = document.getElementById('s1').value;
-    triangle[1] = document.getElementById('s2').value;
-    triangle[2] = document.getElementById('s3').value;
+    triangle[0] = parseInt(document.getElementById('s1').value);
+    triangle[1] = parseInt(document.getElementById('s2').value);
+    triangle[2] = parseInt(document.getElementById('s3').value);
 
     if ( (triangle[0]+triangle[1]) <= triangle[2] || (triangle[1]+triangle[2]) <= triangle[0] || (triangle[2]+triangle[0]) <= triangle[1] ) {
-        alert("cannot form a triangle");
+        alert("your sides cannot form a triangle");
     }else if ( triangle[0] === triangle[1] && triangle[1] === triangle[2] && triangle[2] === triangle[0] ) {
-        document.getElementById('type').innerHTML = "<P>this is an equilateral triangle. All side sof the triangle are equal and the angles are 60.</P>";
+        document.getElementById('type').innerHTML = "<P>This is an equilateral triangle. all sides and angles are equal.</P>";
     }else if ( triangle[0] === triangle[1] || triangle[1] === triangle[2] || triangle[2] === triangle[0] ) {
-        document.getElementById('type').innerHTML = "<p>this is an isosceles triangle. 2 sides and 2 angles are equal</p>";
+        document.getElementById('type').innerHTML = "<p>This is an isosceles triangle. two sides and two angles are equal</p>";
     }else if ( (triangle[0]+triangle[1]) >= triangle[2] || (triangle[1]+triangle[2]) >= triangle[0] || (triangle[2]+triangle[0]) >= triangle[1]){
-        document.getElementById('type').innerHTML = "<p>this is a scalene triangle no sides a equal and none of the angles are equal</p>";
+        document.getElementById('type').innerHTML = "<p>this is a scalene triangle no sides are equal.</p>";
     }else {
         document.getElementById('type').innerHTML = "<h2>Please try again</h2>";
     }
-}
+};
 function trianglereset() {
 document.getElementById('type').innerHTML = "";
 triangle = [];
